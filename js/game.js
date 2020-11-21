bb1d88d0711de81989a9d1464db697e71750330a
Jason Gabriel <jasongbds@gmail.com>
2020-11-21 17:44:48 -0300

    Ajuste Musica, Identação, Icone, Textos

--- js/game.js
+++ js/game.js
@@ -1,22 +1,16 @@
-
 var game = new Phaser.Game(1200, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
+
 var OBJbutton = [], objPortas = [], objPerguntas = [], kill = [], Mensagem = [];
-var cena, prachetas, panfletos, barreiras, objetosInterativos,contatoAtual, Fase = 0, andar = true, spriteFase = Fase - 1,FasePerdeu=7, acao,posicao="R", operacao = 'Load';
+var cena, pranchetas, panfletos, barreiras, objetosInterativos, contatoAtual, Fase = 0, andar = true, spriteFase = Fase - 1, FasePerdeu = 7, acao, posicao = "R", operacao = 'Load';
+var text;
+var button;
 
 function preload() {
-     
 
 }
 
-var text;
-var button;
-var x = 32;
-var y = 80;
-
 function start() {
-
-
-    game.load.audio('audio', 'audio/saber_quem_sou.mp3');
+    game.load.audio('audio', ['audio/clipe_a_bela_e_a_fera.mp3']);
 
     $.getJSON("json/fase.json", function (data) {
         $.each(data, function (key, val) {
@@ -30,8 +24,6 @@ function start() {
         })
     });
 
-    
-    //Load Pranchetas
     cena = game.load.spritesheet('Cena1', 'img/Cenas1-2-3-4-5.png', 2401, 600);
     cena2 = game.load.spritesheet('Cena2', 'img/Cenas6-7-8-9-10.png', 2400, 600);
     game.load.image('MenuPrincipal', 'img/MenuPrincipal.png');
@@ -40,7 +32,7 @@ function start() {
     game.load.image('MenuSobre', 'img/MenuSobre.png');
     game.load.image('MenuComoJogar', 'img/MenuComoJogar.png');
     game.load.image('cadeirante', 'img/cadeirante.png');
-     game.load.image('Enfermeiro', 'img/Enfermeiro.png');
+    game.load.image('Enfermeiro', 'img/Enfermeiro.png');
     game.load.image('Atendimento01', 'img/Atendimento01.png');
     game.load.image('Atendimento02', 'img/Atendimento02.png');
     game.load.image('Recepicionista01', 'img/Recepicionista01.png');
@@ -61,45 +53,37 @@ function start() {
     game.load.image('BotãoConfirmar', 'img/BotãoConfirmar.png');
     game.load.image('resposta', 'img/resposta.png');
     player = game.load.spritesheet('jogador', 'img/sprints.png', 155.8, 170);
-
     game.load.start();
-
-
 }
 
 function loadStart() {
-
-	text.setText("Carregando ....");
-
+    text.setText("Carregando ....");
 }
+
 function fileComplete(progress, cacheKey, success, totalLoaded, totalFiles) {
-
-	text.setText("Arquivos Completos: " + progress + "% - " + totalLoaded + " de " + totalFiles);
-
+    text.setText("Arquivos Completos: " + progress + "% - " + totalLoaded + " de " + totalFiles);
 }
 
 function loadComplete() {
-
     text.setText("Carreamento Completo");
     game.stage.disableVisibilityChange = true;
-    music = game.add.audio('audio');
+    music = game.add.audio('audio', 0.2, true);
     music.play();
-    operacao="MenuPrincipal";
+    operacao = "MenuPrincipal";
     create();
 }
 
 function create() {
 
-   
-
     switch (operacao) {
+
         case "jogo":
             game.physics.startSystem(Phaser.Physics.ARCADE);
 
             DesenharSala(Fase)
 
             game.world.setBounds(0, 0, 2403, 600);
-            //Criando Plataformas
+
             player = game.add.sprite(10, 340, 'jogador');
 
             game.physics.arcade.enable(player);
@@ -109,9 +93,8 @@ function create() {
             player.animations.add('down', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19], 20, true);
 
             player.body.collideWorldBounds = true;
-            /*Adicionando Fisica ao Player*/
+
             player.body.bounce.y = 0.1;
-            //player.body.gravity.y = 2000;
 
             cursors = game.input.keyboard.createCursorKeys();
             game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
@@ -130,43 +113,33 @@ function create() {
             break;
 
         case "Load":
-            game.stage.backgroundColor = '#182d3b';
+            game.stage.backgroundColor = 'rgb(30, 30, 32)';
 
-            //	You can listen for each of these events from Phaser.Loader
+  
             game.load.onLoadStart.add(loadStart, this);
             game.load.onFileComplete.add(fileComplete, this);
             game.load.onLoadComplete.add(loadComplete, this);
-        
-            //	Just to kick things off
-            
-        
-            //	Progress report
-            text = game.add.text(250, 300, 'Prestes a carregar o jogo', { fill: '#ffffff' });
-        
 
-          
+            text = game.add.text(350, 300, 'Prestes a carregar o jogo', { fill: '#ffffff' });
+
             start();
             break;
 
         case "MenuPrincipal":
             CenaMenu = game.add.sprite(0, 0, 'MenuPrincipal');
-            btnJogar = game.add.button(455.5, 260, "btnJogar", ClickBtnJogo, this);
-            btnComoJogar = game.add.button(455.5, 380, "btnComoJogar", ClickBtnComoJoga, this);
-            btnSobre = game.add.button(455.5, 500, "btnSobre", ClickBtnSobre, this);
+            btnJogar = game.add.button(455.5, 210, "btnJogar", ClickBtnJogo, this);
+            btnComoJogar = game.add.button(455.5, 330, "btnComoJogar", ClickBtnComoJoga, this);
+            btnSobre = game.add.button(455.5, 450, "btnSobre", ClickBtnSobre, this);
             kill.push(btnComoJogar);
             kill.push(btnJogar);
             kill.push(btnSobre);
             break;
-        
+
     }
-
-
 
 }
 
-
 function DesenharSala(faseAtual) {
-console.log(faseAtual)
 
     if (faseAtual <= 4) {
         spriteFase++;
@@ -189,13 +162,11 @@ console.log(faseAtual)
         alert("Fim de Jogo!")
     }
 
-
-
     if (faseAtual != 6 && faseAtual != FasePerdeu) {
-        prachetas = game.add.group();
-        prachetas.enableBody = true;
-        var pracheta = prachetas.create(objPortas[faseAtual].prancheta.x, objPortas[faseAtual].prancheta.y, 'prancheta');
-        pracheta.body.immovable = true;
+        pranchetas = game.add.group();
+        pranchetas.enableBody = true;
+        var prancheta = pranchetas.create(objPortas[faseAtual].prancheta.x, objPortas[faseAtual].prancheta.y, 'prancheta');
+        prancheta.body.immovable = true;
     }
 
     for (let position in objPortas[faseAtual].panfletos) {
@@ -213,17 +184,9 @@ console.log(faseAtual)
         var porta = portas.create(objPortas[faseAtual].portas[position].x, objPortas[faseAtual].portas[position].y, 'porta');
         porta.body.immovable = true;
         porta.resposta = objPortas[faseAtual].portas[position].resposta
-        if(faseAtual==6)
-        porta.scale.setTo(1, 0.8);
+        if (faseAtual == 6)
+            porta.scale.setTo(1, 0.8);
 
-    }
-
-    for (let position in objPortas[faseAtual].panfletos) {
-        panfletos = game.add.group();
-        panfletos.enableBody = true;
-        var panfleto = panfletos.create(objPortas[faseAtual].panfletos[position].x, 340, objPortas[faseAtual].panfletos[position].id);
-        panfleto.body.immovable = true;
-        panfleto.panfleto = objPortas[faseAtual].panfletos[position].grande;
     }
 
     for (let position in objPortas[faseAtual].barreira) {
@@ -246,7 +209,6 @@ console.log(faseAtual)
         objetosInterativo.mensagemVisivel = objPortas[faseAtual].objetosInterativos[position].mensagemVisivel;
         objetosInterativo.balao = objPortas[faseAtual].objetosInterativos[position].balao;
         objetosInterativo.id = position;
-        
 
     }
 
@@ -259,53 +221,38 @@ function respondendo(player, porta) {
     this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
     if (this.spaceKey.isDown) {
-        console.log("Resposta: " + porta.resposta);
-
         if (objPerguntas[Fase].respostaCorreta === porta.resposta) {
             lendoResposta(player, porta, true);
         }
         else {
             lendoResposta(player, porta, false);
-            console.log("Errado!");
         }
 
     }
 }
 
 function spawnPlayer(player, porta) {
-    console.log(porta.x)
     player.position.x = porta.x;
-
 }
 
 function lendoPrancheta(player, prancheta) {
     this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
     if (this.spaceKey.isDown) {
         var posicaoX;
         andar = false;
         let texto;
-
         if (player.x > 500)
             posicaoX = player.x;
         else
             posicaoX = 500;
-
         var pranchetaGrande = game.add.sprite(posicaoX - 500, 0, 'pranchetaView');
-
-
-        //  Add and update the score
         var style = { font: "20px ", fill: "black" };
-
         texto = objPerguntas[Fase].enunciado;
-
-        text = game.add.text(posicaoX - 430, 190, texto, style);
-        //var cleanString = text.cleanText(texto);
+        texto = game.add.text(posicaoX - 430, 190, texto, style);
         button = game.add.button(posicaoX - 410, 500, "BotaoSair", ClickSair, this);
-        kill.push(text);
+        kill.push(texto);
         kill.push(pranchetaGrande);
         kill.push(button);
-
     }
 }
 
@@ -314,10 +261,8 @@ function lendoPanfletos(player, panfleto) {
     this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
     if (this.spaceKey.isDown) {
-        var posicaoX;
+        let posicaoX;
         andar = false;
-        let texto;
-
         if (player.x > 500)
             posicaoX = player.x;
         else
@@ -325,7 +270,7 @@ function lendoPanfletos(player, panfleto) {
 
         var panfletoGrande = game.add.sprite(posicaoX - 500, 0, panfleto.panfleto);
         //  Add and update the scor
-        button = game.add.button(posicaoX - (panfletoGrande.width/2), panfletoGrande.height, "BotaoSair", ClickSair, this);
+        button = game.add.button(posicaoX - (panfletoGrande.width / 2), panfletoGrande.height, "BotaoSair", ClickSair, this);
 
         kill.push(panfletoGrande);
         kill.push(button);
@@ -380,8 +325,6 @@ function lendoResposta(player, papel, auth) {
     andar = false;
     let texto, assunto;
     var pranchetaGrande = game.add.sprite(posicaoX - 300, 100, 'resposta');
-
-    //  Add and update the score
     var style = { font: "21px ", fill: "black" };
     if (Fase != FasePerdeu) {
         assunto = game.add.text(posicaoX - 260, 150, "Resposta para este caso:", style);
@@ -389,29 +332,26 @@ function lendoResposta(player, papel, auth) {
     }
     texto = objPerguntas[Fase].respostas[respondido];
     text = game.add.text(posicaoX - 250, 195, texto, style);
-    //var cleanString = text.cleanText(texto);
-
     buttonSair = game.add.button(posicaoX - 200, 400, "BotaoSair", ClickSair, this);
 
     buttonConfirmar = game.add.button(posicaoX, 400, "BotãoConfirmar", function () {
-        if((Fase == FasePerdeu || Fase == 6) && papel.resposta == objPerguntas[Fase].respostaCorreta){
+        if ((Fase == FasePerdeu || Fase == 6) && papel.resposta == objPerguntas[Fase].respostaCorreta) {
             game.world.remove(portas);
             barreiras = [];
             objetosInterativos = [];
             panfletos = [];
             kill = [];
             Mensagem = [];
-            player.x=0;
-            player.y=0;
-            Fase=0;
+            player.x = 0;
+            player.y = 0;
+            Fase = 0;
             spriteFase = Fase - 1;
-            andar = true; 
+            andar = true;
             game.world.remove(cena);
             operacao = 'MenuPrincipal';
             create();
-            
-            
-        }else  if (papel.resposta == objPerguntas[Fase].respostaCorreta) {
+
+        } else if (papel.resposta == objPerguntas[Fase].respostaCorreta) {
             game.world.remove(portas);
             barreiras = [];
             objetosInterativos = [];
@@ -426,7 +366,7 @@ function lendoResposta(player, papel, auth) {
             game.world.remove(portas);
             barreiras = [];
             objetosInterativos = [];
-            prachetas = [];
+            pranchetas = [];
             panfletos = [];
             game.world.remove(cena);
             DesenharSala(FasePerdeu);
@@ -435,30 +375,26 @@ function lendoResposta(player, papel, auth) {
         }
     }, this);
 
-
     kill.push(text);
     kill.push(pranchetaGrande);
     kill.push(buttonSair);
     kill.push(buttonConfirmar);
-
-
 }
 
 function acaoObjeto(player, objeto) {
 
     if (objeto.mensagemVisivel) {
-        console.log(objeto.mensagemVisivel)
         var Balao = game.add.sprite(objeto.balaoX, objeto.balaoY, objeto.balao);
         if (Fase == FasePerdeu)
             Balao.scale.setTo(1.5, 1.5);
         if (Fase == 6)
-            Balao.scale.setTo(1.7,1.5);
+            Balao.scale.setTo(1.7, 1.5);
         var style = { font: "21px ", fill: "black" };
         text = game.add.text(objeto.balaoX + 25, objeto.balaoY + 30, objeto.mensagem, style);
         Mensagem.push(Balao);
         Mensagem.push(text);
         objeto.mensagemVisivel = false
-        contatoAtual=true;
+        contatoAtual = true;
     }
 
 }
@@ -471,7 +407,7 @@ function jogo() {
 
     game.physics.arcade.collide(player, barreiras);
     game.physics.arcade.overlap(player, portas, respondendo, null, this);
-    game.physics.arcade.overlap(player, prachetas, lendoPrancheta, null, this);
+    game.physics.arcade.overlap(player, pranchetas, lendoPrancheta, null, this);
     game.physics.arcade.overlap(player, panfletos, lendoPanfletos, null, this);
     var Contato = game.physics.arcade.overlap(player, objetosInterativos, acaoObjeto, null, this);
 
@@ -480,35 +416,35 @@ function jogo() {
         Mensagem.forEach(function (item) { item.kill(); });
 
     }
-  
+
     if (cursors.left.isDown && andar) {
-        //  Move to the left
+
         player.body.velocity.x = -velocidade;
         player.animations.play('left');
-        posicao="L"
+        posicao = "L"
 
     }
     else if (cursors.right.isDown && andar) {
-        //  Move to the right
+
         player.body.velocity.x = velocidade;
         player.animations.play('right');
-        posicao="R"
+        posicao = "R"
     }
     else if ((cursors.up.isDown) && player.y >= 342.5 && andar) {
-        //  Move to the right
+
         player.body.velocity.y = -velocidade;
         player.animations.play('up');
 
 
     } else if (cursors.down.isDown && andar) {
-        //  Move to the right
+
         player.body.velocity.y = +velocidade;
         player.animations.play('down');
     }
     else {
-        if (posicao=='R'){
+        if (posicao == 'R') {
             player.frame = 10;
-        }else if(posicao=='L'){
+        } else if (posicao == 'L') {
             player.frame = 29;
         }
         player.body.velocity.x = 0;
@@ -518,7 +454,6 @@ function jogo() {
 
     if (player.y < 342)
         player.y = 342
-
 
 }
 
@@ -538,8 +473,8 @@ function update() {
 
             break;
         case "Load":
-       
-        break
+
+            break
 
     }
 
